ada8a823df4ff2034d9dd9eb0b7f757d9d2a71df
LaoTseu <eric.beaudoin@gmail.com>
2006-12-11 06:05:08 +0000

    Fixed a stupid typo I made
    git-svn-id: svn://svn.wowace.com/wow/all-played-laotseu/mainline/tmp/trunk/AllPlayed@36 0c53f26f-d59f-4dd4-8d37-a1ff69c72365

--- AllPlayed.lua
+++ AllPlayed.lua
@@ -203,8 +203,8 @@ function AllPlayed:OnEnable()
     
     -- Hook the functions that need hooking
     -- (hook removal is done automagicaly by ACE)
-    self:Secure("Logout", true)
-    self:Secure("Quit",   true)
+    self:Hook("Logout", true)
+    self:Hook("Quit",   true)
   
     -- Initialize values that don't change between reloads
     self.faction    = UnitFactionGroup("player")
