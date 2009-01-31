f4ba683a96553436828340bfaa05c4b88b1f3b31
LaoTseu <eric.beaudoin@gmail.com>
2009-01-31 00:24:50 +0000

    - Trying to get the script to generate a proper ZIP file
    git-svn-id: svn://svn.wowace.com/wow/all-played-laotseu/mainline/trunk@155 0c53f26f-d59f-4dd4-8d37-a1ff69c72365

--- AllPlayed.lua
+++ AllPlayed.lua
@@ -1,7 +1,6 @@
 ﻿-- AllPlayed.lua
 -- $Id$
 
-
 if not AllPlayed_revision then AllPlayed_revision = {} end
 AllPlayed_revision.main	= ("$Revision$"):match("(%d+)")
 AllPlayed_revision.toc  = GetAddOnMetadata("AllPlayed", "Version"):match("%$Revision:%s(%d+)")
